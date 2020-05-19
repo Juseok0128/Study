7903b98583be22ae0b1a9d914b5f0e04d2ef44eb
Juseok0128 <dsaeref@gmail.com>
2020-05-19 22:05:51 +0900

    처음 올려봅니다..
    냉무

---
+++ Java/src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        System.out.println("hello world");
+        System.out.println("자동정렬 단축키 ctrl+alt+L");
+        print.sori();
+        System.out.println(a);
+    }
+}
+
+class print{
+    public static void sori(){
+        System.out.println("으악");
+    }
+
+}
