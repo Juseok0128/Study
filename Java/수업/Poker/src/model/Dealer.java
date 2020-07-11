c9d8d35e3e0d6ad41d2c2dd747a538a65da31b97
Juseok0128 <dsaeref@gmail.com>
2020-07-11 19:50:06 +0900

    7월 11일
    반복되는 부분 묶음

--- Java/수업/Poker/src/model/Dealer.java
+++ Java/수업/Poker/src/model/Dealer.java
@@ -70,30 +70,31 @@ public class Dealer {
         }
         for (int i = numCount.length - 1; i >= 0; i--) {     //트리플
             if (numCount[i] == 3) {
-                BigDecimal tmp = new BigDecimal(String.valueOf(i)).divide(new BigDecimal("100")).add(new BigDecimal("3.01"));
-                rank = tmp.doubleValue();
+                rank = this.getRankScore(i, 3.01);
                 if (rank == 3.01) return 3.14;      //a점수 보정
                 return rank;
             }
         }
         for (int i = numCount.length - 1; i >= 0; i--) {     //원페어
             if (numCount[i] == 2) {
-                BigDecimal tmp = new BigDecimal(String.valueOf(i)).divide(new BigDecimal("100")).add(new BigDecimal("2.01"));
-                rank = tmp.doubleValue();
+                rank = this.getRankScore(i, 2.01);
                 if (rank == 2.01) return 2.14;      //a점수 보정
                 return rank;
             }
         }
         for (int i = numCount.length - 1; i >= 0; i--) {     //탑
             if (numCount[i] == 1) {
-                BigDecimal tmp = new BigDecimal(String.valueOf(i)).divide(new BigDecimal("100")).add(new BigDecimal("1.01"));
-                rank = tmp.doubleValue();
+                rank = this.getRankScore(i, 1.01);
                 if (rank == 1.01) return 1.14;      //a점수 보정
                 return rank;
             }
         }
         System.out.println("아무 조건에 해당하지 않습니다.");
         return 0;
+    }
+    static double getRankScore(int cardNumber, double defalutScore){
+        BigDecimal tmp = new BigDecimal(String.valueOf(cardNumber)).divide(new BigDecimal("100")).add(new BigDecimal(String.valueOf(defalutScore)));
+        return tmp.doubleValue();
     }
 
     public void printRank(double rank) {
@@ -107,65 +108,32 @@ public class Dealer {
             return;
         }
         if (rank > 3) {
-            if (rank == 3.14) {
-                System.out.println("A트리플");
-                return;
-            } else if (rank == 3.13) {
-                System.out.println("K트리플");
-                return;
-            } else if (rank == 3.12) {
-                System.out.println("Q트리플");
-                return;
-            } else if (rank == 3.11) {
-                System.out.println("J트리플");
-                return;
-            } else {
-                BigDecimal tmp = new BigDecimal(String.valueOf(rank)).subtract(new BigDecimal("3")).multiply(new BigDecimal("100"));
-                printRank = tmp.intValue();
-                System.out.println(printRank + "트리플");
-                return;
-            }
+            rank = new BigDecimal(String.valueOf(rank)).subtract(new BigDecimal("3")).doubleValue();
+            System.out.println(this.getScoreMark(rank)+"트리플");
         }
         if (rank > 2) {
-            if (rank == 2.14) {
-                System.out.println("A원페어");
-                return;
-            } else if (rank == 2.13) {
-                System.out.println("K원페어");
-                return;
-            } else if (rank == 2.12) {
-                System.out.println("Q원페어");
-                return;
-            } else if (rank == 2.11) {
-                System.out.println("J원페어");
-                return;
-            } else {
-                BigDecimal tmp = new BigDecimal(String.valueOf(rank)).subtract(new BigDecimal("2")).multiply(new BigDecimal("100"));
-                printRank = tmp.intValue();
-                System.out.println(printRank + "원페어");
-                return;
-            }
+            rank = new BigDecimal(String.valueOf(rank)).subtract(new BigDecimal("2")).doubleValue();
+            System.out.println(this.getScoreMark(rank)+"원페어");
         }
         if (rank > 1) {
-            if (rank == 1.14) {
-                System.out.println("A탑");
-                return;
-            } else if (rank == 1.13) {
-                System.out.println("K탑");
-                return;
-            } else if (rank == 1.12) {
-                System.out.println("Q탑");
-                return;
-            } else if (rank == 1.11) {
-                System.out.println("J탑");
-                return;
-            } else {
-                BigDecimal tmp = new BigDecimal(String.valueOf(rank)).subtract(new BigDecimal("1")).multiply(new BigDecimal("100"));
-                printRank = tmp.intValue();
-                System.out.println(printRank + "탑");
-                return;
-            }
+            rank = new BigDecimal(String.valueOf(rank)).subtract(new BigDecimal("1")).doubleValue();
+            System.out.println(this.getScoreMark(rank)+"탑");
         }
         System.out.println("해당하는 족보가 없습니다.");
+    }
+    String getScoreMark(double rank){
+        if (rank == 0.14) {
+            return "A";
+        } else if (rank == 0.13) {
+            return "K";
+        } else if (rank == 0.12) {
+            return "Q";
+        } else if (rank == 0.11) {
+            return "J";
+        } else {
+            BigDecimal tmp = new BigDecimal(String.valueOf(rank)).multiply(new BigDecimal("100"));
+            int prnitTmp = tmp.intValue();
+            return String.valueOf(prnitTmp);
+        }
     }
 }
